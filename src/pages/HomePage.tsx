68952fddc020d0f8958ac650231793f56fc81511
nick3842 <nick3842@protonmail.com>
2022-07-09 20:57:40 -0500

    Add homepage buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,27 @@
+import { CreateTodoButton } from './CreateTodoButton'
+import { DeleteTodoButton } from './DeleteTodoButton'
+import { ListTodosButton } from './ListTodosButton'
+
 export const HomePage = () => {
   return (
-    <div>
-      HomePage
-    </div>
+    <>
+      <div className="h-2/3">
+        <div className="text-center grid place-items-center mx-auto text-4xl text-red-500">
+          <div>
+            This is the home page
+            <br></br>
+            <span className="underline">
+              Options
+            </span>
+          </div>
+          <CreateTodoButton />
+          <ListTodosButton />
+          {/* <UpdateTodoModal /> */}
+          <DeleteTodoButton />
+          {/* <CompleteTodoModal /> */}
+          {/* <ShowCompletedTodosModal /> */}
+        </div>
+      </div>
+    </>
   )
 }
