f3a94cc14045395d889d4a76ca0f2010a06b0628
nick3842 <nick3842@protonmail.com>
2022-07-09 21:00:37 -0500

    Add delete todo modal template

---
+++ src/pages/DeleteTodoButton.tsx
@@ -0,0 +1,13 @@
+
+export const DeleteTodoButton = () => {
+  return (
+    <div className="mt-4">
+      <button
+        className="border-4 py-2 px-2 border-red-500 rounded"
+        onClick={() => console.log("hi")}
+      >
+        Delete Todo
+      </button>
+    </div>
+  )
+}
