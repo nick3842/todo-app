2242da5ca568ee5d231bc5c11d78a4e2a55acc44
nick3842 <nick3842@protonmail.com>
2022-07-09 20:58:36 -0500

    Implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { HomePage } from './pages/HomePage'
 function App() {
   return (
     <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="create" element={<CreateTodoButton />} />
+      <Route path="showtodos" element={<ListTodosButton />} />
+    </Routes>
   )
 }
 
